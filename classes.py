887f4d43beed965a18d3047523ac2570b92008d8
m-gec <mustafa.gec@laetottenham.ac.uk>
2024-12-15 21:31:55 +0000

    fytdydydtiuvyy

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 import numpy as np
+from noise import pnoise2 #noise must be installed via pip3
 
 '''
 screen = np.zeros((5, 5))
@@ -8,31 +9,104 @@ array = np.array([[23, 46, 85],
 screen[:3, :3] = array
 print(screen)'''
 
+
+#map region calculation functions ---------------------------------------------------------------------------------------------------------------
+
+def noise(x, y, map):
+    height, width = map.dimensions
+    scale = 8
+    result = pnoise2(x / scale, y / scale, octaves=1, persistence=0, lacunarity=0, repeatx=width, repeaty=height, base=7)
+    #print((result))
+    return 1 - min(round(result * 10), 1)
+
+
+def within_castle(x, y, y_sf, a, b, r, depth): # (x-a)**2 + ((y-b) * y_sf)**2 <= r**2        checks for main body, then each tower
+    
+    if depth == 0:#only apply on the first iteration of the function
+        #get x and y relative to the centre (int of y for unique curvature)
+        x, y = x - a, int((y-b) * y_sf)
+    depth += 1
+
+     #do not go any deeper for the towers
+    if depth == 2: return ((x)**2 + (y)**2 <= r**2) #tower shape calculation
+
+    #lies_within_main_body = False#((x)**2 + (y)**2 <= r**2) #main body shape calculation
+    path_thickness = 4.5
+    gradient = 0.8
+
+    #check for inner body & 4 connecting paths clamped at map edges
+    #((x)**2 + (y)**2 <= (r / 1.5)**2) or ---- former circle
+
+    path_one = ((-gradient * x + path_thickness >= y and -gradient * x - path_thickness <= y) and x > -20 and x < 20 and y > -20 and y < 20)
+    path_two = ((gradient * x + path_thickness >= y and gradient * x - path_thickness <= y) and x > -20 and x < 20 and y > -20 and y < 20)
+    lies_within_main_body = path_one or path_two
+    if lies_within_main_body: return True
+
+    #adjust variables for the smaller towers
+    dist_sf = 2
+    radial_sf = 2.4
+    r /= radial_sf
+
+    within_north_towers = within_castle(x - (dist_sf * r), y + (dist_sf * (r / y_sf)), y_sf, a, b, r, depth) or within_castle(x + (dist_sf * r), y + (dist_sf * (r / y_sf)), y_sf, a, b, r, depth)
+    within_south_towers = within_castle(x - (dist_sf * r), y - (dist_sf * (r / y_sf)), y_sf, a, b, r, depth) or within_castle(x + (dist_sf * r), y - (dist_sf * (r / y_sf)), y_sf, a, b, r, depth)
+
+    return within_north_towers or within_south_towers
+
+
+#variables -------------------------------------------------------------------------------------------------------------------------------------
+
 tiles = [
     "  ",
     "██",
     "▓▓",
-    "▒▒"
+    "⏏ ",#ℹ۩۞
+    "▒▒",
+    "  ",
+    "░░",
+    "",
+    "⌺ ",
+    "♝ ",
+    "♜ ",
+    "♞ ",
+    "✠ ",
+    "✤ ",
+    "✦ ",
+    "ጨጭጮ",
+    "౷"
 ]
+
+
+
+
+#classes ---------------------------------------------------------------------------------------------------------------------------------------
 
 class Screen:
     def __init__(self, passed_height, passed_width):
         self.dimensions = (passed_height, passed_width)
         self.grid = np.zeros((passed_height, passed_width))
     
-    def add_map_to_screen(self, map):
+    def add_elements_to_screen(self, elements): #0 = map, 1 = player, 2 = inventory, 3 = enemies, 4 = npc merchants, 5 = interactable items
         screen = self.grid
 
-        map_array = map.array
-        map_width, map_height = map.dimensions
+        map, player, inventory, enemies, merchants, items = elements
 
-        screen[:map_width, :map_height] = map_array
+        #add map
+        map_height, map_width = map.dimensions
+        screen[:map_height, :map_width] = map.array
+
+        #add player
+        player_x, player_y = player.position
+        screen[player_x][player_y] = 3
+
+
+
         self.grid = screen
 
     def display(self):
         screen = self.grid
         for row in screen:
-            print(row)
+            print("".join([(((i < 97) and tiles[min(int(i), len(tiles) - 1)]) or f"{chr(int(i))}") for i in row]))
+            #text occupies exactly half the width of the generic pixel
 
 
 class Map:
@@ -42,25 +116,63 @@ class Map:
     
     def generate_floor(self, floor_number):
         height, width = self.dimensions
-        map_array = np.array([])
 
+        map_array = []
         for y in range(height):
-            map_row = np.array([])
-            #for x in range(width):
-            match floor_number:
-                case 1:
-                    #-----------------------------------------------------------------------------
-                    map_row = [tiles[1] * width]
-                    print(map_row[0])
-                    #-----------------------------------------------------------------------------
-                case 2:
-                    #-----------------------------------------------------------------------------
-                    pass
-                    #-----------------------------------------------------------------------------
-                case 3:
-                    #-----------------------------------------------------------------------------
-                    pass
-                    #-----------------------------------------------------------------------------
+            map_row = []
+            for x in range(width):
+                tile_color = 1
+                match floor_number:
+                    case 1: #
+                        #basic circle ----------------------------------------------------------------
+                        
+                        if (x-(width // 2))**2 + ((y-(height // 2)) * 1.2)**2 <= 23**2:
+                            tile_color = 2
+                            if (x-(width // 2))**2 + ((y-(height // 2)) * 1.2)**2 <= 21.75**2:
+                                if noise(x, y, self) == 0:
+                                    #additional obstacles
+                        
+                                    dark_tile, light_tile = 4, 6
+                                    tile_color = (x - 1) % 2 == 0 and (y % 2 == 0 and dark_tile or light_tile) or (y % 2 == 0 and light_tile or dark_tile)
+                                else:
+                                    tile_color = 1
+                        else:
+                            tile_color = 1
+                        #-----------------------------------------------------------------------------
+                    case 2: #castle
+                        #-----------------------------------------------------------------------------
+                        if within_castle(x, y, 1.2, width // 2, height // 2, 22, 0): 
+                            #check for static objects (interior walls, doors)
 
-            #map_array += [map_row]
+                            #render a checkerboard floor pattern
+                            tile_color = (x - 1) % 2 == 0 and (y % 2 == 0 and 2 or 5) or (y % 2 == 0 and 5 or 2)
+                        else:
+                            tile_color = 1 #black tile
+                        
+                        #-----------------------------------------------------------------------------
+                    case 3:
+                        #-----------------------------------------------------------------------------
+                        pass
+                        #-----------------------------------------------------------------------------
+                map_row += [tile_color]
+            map_array += [map_row]
         return map_array
+    
+
+class Player:
+    def __init__(self):
+        self.position = (22, 30) #initial player position
+        self.inventory = Inventory()
+    
+    def move(self, direction, map):
+        pass
+        #either move until an obstruction is reached, or move a set unit
+
+
+class Inventory:
+    def __init__(self):
+        self.items = []
+        self.inventory_screen = self.generate_inventory_screen()
+
+    def generate_inventory_screen(self):
+        return
--- main.py
+++ main.py
@@ -3,35 +3,132 @@ import numpy as np
 from random import randint as random
 import os
 import time
-os.system('clear')
+import shutil
+import sys
+
+
+#inital terminal sizing configuration -----------------------------------------------------------------------------------------------------------------------------------
+
+accepted_columns, accepted_lines = 160, 35
+
+def validate_terminal_dimensions(accepted_columns, accepted_lines):
+    columns, lines = shutil.get_terminal_size()
+    if (columns < accepted_columns) or (lines < accepted_lines):
+        print((columns < accepted_columns) and f"terminal width is too narrow ({columns}/{accepted_columns}), make it wider" or f"terminal height is too low ({lines}/{accepted_lines}), make it taller")
+        time.sleep(0.5)
+        #os.system('clear')
+        return False
+    else:
+        os.system('clear')
+        return True
+
+while True:
+    os.system('clear')
+    if validate_terminal_dimensions(accepted_columns, accepted_lines): break
+
+#sys.exit()
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#[settings]:
 
 #height and width of the screen
-height, width = 45, 60
+height, width = 45, 80
 
 #height and width of the map
-map_height = 30
-map_width = 30#np.floor(map_height * np.pi * 0.5)
+map_height = 45
+map_width = 60
 
 chars = [
     "❂"
 ]
 
+#instance configuration ------------------------------------------------------------------------------------------------------------------------------------------------
 
+player = Player()
 
-#main loop ------------------------------------------------------------------------------------
+#main loop -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 while True:
     #clear the last screen
     os.system('clear')
 
-    screen = Screen(height, width)
-    map = Map(height, width, 1)
+    #validate terminal dimensions (disabled for performance)
+    #if not validate_terminal_dimensions(accepted_columns, accepted_lines): continue
 
-    #screen.add_map_to_screen(map)
-    #screen.display()
+    #generate instances -------------------------------------------------------------------------
+
+    screen = Screen(height, width)
+    map = Map(map_height, map_width, 1)
+
+    #display elements ---------------------------------------------------------------------------
+
+    screen.add_elements_to_screen([map, player, 1, 1, 1, 1])#error origin
+    screen.display()
+
+    break
+
+    #keybinds -----------------------------------------------------------------------------------
 
     pressed_key = input()
+    match pressed_key.lower():
+        #movement keybinds-----------------------------
+        #top left is (0, 0)
+        case "w":
+            player.move(np.array([0, -1]), map)
+        case "a":
+            player.move(np.array([-1, 0]), map)
+        case "s":
+            player.move(np.array([0, 1]), map)
+        case "d":
+            player.move(np.array([1, 0]), map)
+
+        #action keybinds-------------------------------
+        
+        case "e": #interacting with items
+            pass
+        case "y": #"yes" response to npcs
+            pass
+        case "n": #"no" response to npcs
+            pass
+        case " ": #attack
+            pass
+
+        #end the program-------------------------------
+
+        case "q":
+            break
 
     #wait the refresh period
-    time.sleep(60**-1)
-    break
+    fps = 5
+    time.sleep(fps**-1)
+
+
+
+
+
+
+
+
+'''
+current screen gets displayed
+player 
+'''
+
+'''
+player drills through walls
+player can only drill in a straight line but move freely in open air
+if their path intersects with an enemy npc, they lose a life and the level resets
+if it insersects with a merchant, they can purchase or sell goods, then must drill perpendicularly to continue (npcs cannot be drilled)
+they must reach the exit and touch it, sometimes requiring a key
+'''
+
+
+
+
+
+
+
+
+
