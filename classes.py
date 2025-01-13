f4cb0a49eccf1eb916b1c99a76b5cd34ece497ea
m-gec <mustafa.gec@laetottenham.ac.uk>
2025-01-13 07:21:23 +0000

    woghewrgweughorwehg

--- classes.py
+++ classes.py
@@ -1,15 +1,19 @@
+import multiprocessing.shared_memory
 import numpy as np
 from noise import pnoise2
+from random import randint as random
+import multiprocessing
+import time
+import sys
+import os
 
-
-
+seed = random(0,100)
 #map region calculation functions ---------------------------------------------------------------------------------------------------------------
 
 def noise(x, y, map):
     height, width = map.dimensions
     scale = 8
-    result = pnoise2(x / scale, y / scale, octaves=1, persistence=0, lacunarity=0, repeatx=width, repeaty=height, base=7)
-    #print((result))
+    result = pnoise2(x / scale, y / scale, octaves=1, persistence=0, lacunarity=0, repeatx=width, repeaty=height, base=seed)
     return 1 - min(round(result * 10), 1)
 
 def convert_to_floor_tile(x, y, array):
@@ -26,6 +30,8 @@ tiles = [
     "  ",
     "░░",
     "",
+    "⚑ ",
+    "⚠ ",
     "⚿ ",
     "♝ ",
     "♜ ",
@@ -35,11 +41,11 @@ tiles = [
     "✦ ",
     "♚ ",
     "♛ ",
-    "ጨጭጮ",
     "౷",
-    "⚠ ",
     "⚑ ",
-    "⚐ "
+    "⚐ ",
+    "♡",
+    "♥"
 ]
 
 
@@ -49,29 +55,70 @@ class Screen:
     def __init__(self, passed_height, passed_width):
         self.dimensions = (passed_height, passed_width)
         self.grid = np.zeros((passed_height, passed_width))
-    
-    def add_elements_to_screen(self, elements): #0 = map, 1 = player, 2 = inventory, 3 = enemies, 4 = npc merchants, 5 = interactable items
-        screen = self.grid
-
-        map, player, inventory, enemies, merchants, items = elements
-
-        #add map
-        map_height, map_width = map.dimensions
-        screen[:map_height, :map_width] = map.array
-
-        #add player
-        player_x, player_y = player.position
-        screen[player_x][player_y] = 3
-
-
-
-        self.grid = screen
 
     def display(self):
         screen = self.grid
         for row in screen:
             print("".join([(((i < 97) and tiles[min(int(i), len(tiles) - 1)]) or f"{chr(int(i))}") for i in row]))
             #text occupies exactly half the width of the generic pixel
+    
+    def add_elements_to_screen(self, elements, maps, current_map): #0 = map, 1 = player, 2 = sidescreen, 3 = enemies, 4 = npc merchants, 5 = interactable items
+        screen = self.grid
+
+        map, player, sidescreen, enemies, merchants, items = elements
+
+        #add map
+        map_height, map_width = map.dimensions
+        screen[:map_height, :map_width] = map.array
+
+        #add sidescreen
+        sidescreen_height, sidescreen_width = sidescreen.dimensions
+        screen[:sidescreen_height, map_width : (map_width + sidescreen_width)] = sidescreen.array
+
+        #add player
+        player_x, player_y = player.position
+
+        #check if a kill block or exit flag is about to be stepped on
+        if screen[player_x][player_y] == 8:
+            #os.system('clear')
+            #print("hit")
+            #sys.exit()
+            #increment map and reset player position
+            player.position = (22, 30) #centre
+            self.grid = screen
+            if current_map != 4:
+                current_map += 1
+                return maps[current_map], current_map
+            else:
+                #game ends
+                os.system('clear')
+                print("you won")
+                sys.exit()
+        elif screen[player_x][player_y] == 9:
+            #game ends
+            os.system('clear')
+            print("(you lost)\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⠤⠶⢶⠞⠿⠿⠿⡿⢿⠿⣷⢶⡶⣤⣤⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠤⠖⠚⠉⡁⠡⠀⡀⠌⠐⣀⢊⡡⡙⡜⢬⢣⣛⡼⣣⢟⡵⣞⡶⣯⣟⣯⢿⡶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠚⠉⢀⠠⠐⠈⡀⡐⠀⡁⠐⢂⠱⣈⠆⡱⡑⢎⢣⠳⣌⠷⣹⢮⣝⠾⣽⣳⢟⡾⣯⡿⣿⣽⣿⣿⣯⣶⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠞⠋⠀⠀⠀⠀⡀⠠⠐⠠⠐⠀⠂⠄⠃⡈⠐⠠⢊⠱⡘⢌⢣⠹⣌⠳⡭⣞⡼⣻⠵⣯⠿⣽⣳⢿⣻⣾⢿⣽⣿⣿⣿⣮⣵⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣟⠋⠀⠀⠀⠀⠀⠀⡐⠀⡁⠄⠁⠄⠡⢈⠀⠂⠄⠉⠤⢁⠆⡩⢘⠢⡝⣌⢳⡱⢎⡷⣭⣛⢷⣻⡽⣯⣟⣯⣿⢿⣻⣿⣿⣿⣿⣿⣿⣿⣷⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠈⠑⠲⢤⣄⣀⡀⠠⠐⠀⠄⡈⠐⢈⠠⠀⠌⠐⠈⡐⠠⠈⠤⢡⢉⠖⡱⢌⡣⣝⠺⣴⢣⣏⡟⣾⢽⣳⢯⣟⣾⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠉⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣷⣶⣥⣔⡀⠡⢀⠐⠈⠠⢈⠐⠀⠄⡉⠐⢂⢡⢊⡱⢊⠴⣊⠷⣡⢟⡼⣹⡞⣯⣟⣯⣟⣷⡿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡈⠙⠿⣿⣿⣷⣦⣬⡀⠡⠀⠂⡁⠐⡀⠑⣈⠢⣘⠰⣉⢖⡩⢞⡱⣎⢷⣣⢟⣧⣟⡾⣽⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣗⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⠟⠁⢀⡰⠋⠀⠀⠀⠀⠀⠀⠀⠀⡀⢁⠀⠢⠤⣄⡉⠻⢿⣿⣿⠷⠈⠐⡀⢁⠐⡐⢀⠒⣄⠓⡌⢦⡙⣬⠳⡜⣧⣛⣮⣷⣾⣿⣿⣿⣿⢿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠋⢀⢐⡞⠁⠀⢀⡠⠴⠒⠋⠉⠉⠉⠉⠉⠓⠶⣤⣀⠉⠓⢦⡈⠀⠄⡈⠐⠀⠄⢂⠐⡈⢒⡠⢙⡘⢦⠱⣊⠗⣿⣿⣿⣿⣿⠿⢛⣯⣷⡾⠟⠋⠉⠉⠉⠉⠉⠙⠛⠿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠁⡐⢠⠎⠀⡠⠒⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣷⣄⠙⢦⡐⠀⠡⢈⠀⢂⠐⡐⢢⠐⣅⠚⡤⢛⢬⡛⣴⢫⣿⠟⣡⣾⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⠐⠠⠏⣠⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣦⣤⣀⠀⠈⠻⣷⡄⠹⣆⠁⠠⢈⠀⢂⠔⠢⡑⢌⡚⠴⡩⢖⡹⢦⡿⢃⣾⣿⠋⠀⠀⠴⣶⣾⣷⣶⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣯⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⢀⠠⠁⢂⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡆⠈⣹⣿⣿⣿⣿⣷⡄⠀⠹⣿⣄⠸⣆⠁⠠⢈⠄⢊⠱⣈⠲⢌⠣⣕⢫⣜⡿⣱⣿⡟⠁⠀⣤⠀⠀⣸⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣷⣃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠁⠠⢐⡠⣿⡁⣀⣀⣤⠤⠤⠤⠤⠤⠤⣤⣼⠿⠿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠈⢿⣆⠀⠠⢁⢂⠘⡄⠣⢌⠲⣉⠞⣰⢣⠾⣳⣿⠏⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣤⣤⣤⣤⣤⣤⣄⣀⣀⢹⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⣌⠱⣦⠷⠟⠋⢁⠀⡀⠄⠂⡐⢀⠂⠐⠤⠤⣤⣤⣀⡀⠈⠉⠙⠻⣇⣀⠀⠀⠈⢻⣦⠁⢂⠆⡌⠰⠩⢌⡱⢸⡸⢥⣋⣾⣿⠋⠀⠀⢀⣀⣿⡿⠟⠛⣉⣩⣭⣭⣷⣶⣶⣾⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⣁⢂⠒⠤⡁⡄⢂⡄⢢⡐⣤⢡⡄⢦⣤⣡⢤⡔⣤⢬⡭⣭⣛⣓⠶⢤⣤⣈⡛⠶⣤⣈⣿⣇⠐⡌⢰⠩⡘⠦⣑⢣⢎⡳⣼⣿⠇⣠⣴⣾⣻⣭⣶⢶⣟⣻⣿⣭⣭⣷⣾⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡗⣌⠢⡉⢆⠴⠨⡅⢎⠵⣸⢰⢣⡝⣣⢎⡵⣋⠾⣩⢞⡱⢯⡝⣯⣟⡿⣶⣻⢿⡿⣦⢍⣛⠛⢦⠘⡤⢣⠙⢦⢩⠖⣭⢲⣛⢿⣻⣟⣷⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡜⣆⠣⡍⢆⠣⣓⠸⢌⠲⡡⠎⢦⠱⢌⠊⡔⣉⠒⣡⠊⡕⢊⠜⡰⠌⡓⠥⡋⢎⡱⢃⠞⡤⢋⠦⣙⠰⢣⢹⡘⣎⡝⢦⡻⣜⢯⡳⣟⡾⣽⣻⣟⣿⣻⢿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡼⣌⠳⡜⣌⠣⢆⠣⣌⠱⣠⠉⡔⣈⢂⠱⠐⡠⠡⢀⠒⠠⠌⡠⢁⠂⡅⢂⡑⢂⠆⡍⢲⡈⡕⢪⡔⣍⣣⢧⣙⣦⣝⣮⢳⡝⣮⢷⣹⡞⣷⡽⣞⡷⣯⣟⣿⣽⣾⢿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⢲⡭⢳⡍⢶⡹⣌⠳⣄⠳⣀⢓⡰⣀⠎⡰⢡⠐⡡⢌⠢⡑⢂⠔⡂⢅⠢⡅⠎⡆⣍⠲⣡⡶⠟⣛⣩⣭⣴⣦⣼⣤⣮⣭⣛⣻⢾⣧⣷⢻⣧⢿⣽⣻⢷⣯⣟⣾⢿⣿⣻⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢼⣿⢧⣛⢧⣞⡣⣗⢬⠳⣌⠳⣌⠦⡑⢆⡚⡰⣁⢚⡐⢢⡑⡘⢤⠚⣄⢋⠴⣡⢋⠔⣢⡿⢋⣴⣾⠟⠋⠉⠉⠹⣿⣟⢿⠹⡟⠻⣷⣮⢻⣟⣾⣻⢾⣽⣻⣞⣯⣿⢿⣻⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢺⣿⢯⣞⣳⢎⡷⣎⣏⢷⣊⠷⣘⠶⡙⢦⡱⠱⣌⠦⡙⢦⠱⣉⢆⠳⣌⡚⢦⡃⢎⣼⢻⣴⠿⢿⡋⠀⠀⠀⠀⠀⠹⡏⠀⠀⠀⠀⠘⢿⣷⣹⣾⡽⣟⣯⣷⣻⣯⣿⡿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣟⡾⣭⣟⢾⡵⢮⡳⣭⢏⡧⣏⡝⣦⣙⠳⣌⠶⣙⢦⢛⡬⢎⡳⡬⡝⣦⢣⣾⣳⠟⠁⠀⠘⡅⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠈⣿⣷⣻⣟⡿⣿⡷⣯⢷⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣯⣟⡷⣯⣟⡾⣏⡷⡽⣎⢷⡹⣞⢦⣝⣣⡝⢮⡵⣊⠷⣼⣇⢿⣱⣹⢇⣻⣾⠋⠀⠀⠀⠀⠁⠀⠀⠀⠀⢀⠀⡁⢀⠀⡀⠀⠀⠀⢸⢻⣷⢿⣟⣿⣿⢿⣟⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣯⢿⣳⣯⢿⡽⣽⢯⣽⣫⢷⣹⡞⣼⡱⣞⢧⡳⣭⢻⣿⣾⣻⢧⡟⣼⣿⣿⣷⣾⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢿⣿⣿⣿⣿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⢿⣽⣯⣿⣽⣻⢾⣭⣟⣧⣟⣧⣟⢮⡷⣹⢮⣿⣿⣷⣟⣾⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣫⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣷⣿⢾⣯⡿⣾⡽⣞⡾⣵⣞⣯⢷⢯⣿⣟⣾⣿⣹⣷⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣾⣿⣿⣽⣷⡿⣯⣟⣷⣻⣞⣯⣟⣾⢿⣿⡿⣿⣳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢏⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⢿⣽⣾⣷⣻⣾⣽⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡎⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣽⣿⣿⣿⣿⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣷⣿⣻⣾⢿⣟⣿⣿⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣯⣿⢿⣿⣻⣯⣿⢿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣻⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣹⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣟⣿⣟⣯⡿⣟⣿⣻⣽⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣷⣿⣿⣿⣿⣟⣿⣾⣻⢷⣯⢿⣽⣻⣽⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡼⣿⣿⣿⣯⣿⣿⢯⣿⢾⣻⢾⣝⣻⢮⡟⣞⡷⣿⠿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⣿⡿⣽⣟⡿⣽⢏⡟⡾⣜⢧⡻⣜⣯⣾⣿⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⡖⠦⢤⡀⢀⣀⡤⣶⠟⠋⣉⡹⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣻⣟⡾⣝⢧⡻⣜⣣⡝⣶⣹⡾⠿⣉⣭⣉⢹⢿⣶⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⠋⠁⠀⠉⠉⠛⠻⠽⢿⣿⣶⡶⠿⠟⠔⠒⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⠿⢿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⠻⢿⠛⠛⣿⣿⣿⡿⣟⣧⢻⠼⣩⢞⡱⢞⡴⣹⡾⢟⣤⣤⣄⡀⠈⠹⠦⢀⣈⡉⠛⠻⣶⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠿⣿⣿⣿⣿⡿⣿⡀⡴⠀⠀⠀⠀⢹⠀⠀⠀⠀⠈⣅⠀⠀⠀⠀⠹⡄⠀⠀⠀⠸⠀⠀⠀⠀⠘⡆⢰⣿⢿⡿⣽⣛⢆⢫⡜⢧⡞⣭⣿⣾⠿⠗⠚⠋⠉⠉⠉⠃⠓⠲⠤⡬⢿⡷⣤⡤⠿⠿⠗⠒⠒⠠⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣷⣀⠀⢀⠀⣿⠀⠀⠀⠀⠀⣱⠀⠀⠀⠀⠀⡇⠀⠀⠀⢀⣇⠀⠀⠀⣆⣿⣿⠿⢹⠛⣤⣙⢮⣧⣿⣯⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡶⣾⡿⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣛⠶⡤⣄⢀⠀⡀⠀⠀⠀⠀⢀⠺⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⠦⣤⣤⣀⣼⣿⣦⣀⣀⣄⣿⣿⣦⣤⣦⣿⣿⣷⣾⡿⢿⠫⠉⢌⣰⣻⣶⣿⣿⣿⣿⢋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠁⠈⠙⢿⡒⠤⣤⣤⣀⣀⣰⣛⣋⣽⠷⢾⣭⣵⣊⣎⠳⣔⢢⡒⣌⠲⣌⢳⢫⣿⣿⣿⣿⣾⣽⣟⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣾⣿⣷⣾⣶⣶⢯⠛⡉⠊⢁⢈⣠⢴⣞⣯⣿⡿⢟⢯⣱⣿⢳⢎⡲⢄⢢⡀⣄⢠⣄⠲⡜⡼⣹⢷⣻⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣌⣿⣷⣾⣷⠀⠀⢀⣽⠿⣟⣉⡡⣄⡀⠀⠀⠈⢛⣿⣷⣼⣤⡓⣌⠳⡘⢣⢋⣿⣧⢏⡟⣿⣻⢿⣾⣷⣯⣯⡽⣍⠯⡹⠉⠏⠑⠉⠌⠁⢁⣈⣠⣴⣰⣷⣾⣿⣿⡿⣟⡟⢧⡹⢎⢖⣿⡏⢓⠮⡱⢋⠖⡱⢌⣣⣎⣷⣽⠶⠷⠞⠻⢯⣛⣿⢦⡀⠀⠀⢀⣀⣀⣤⣾⣿⣿⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡿⢻⣿⣶⣤⠋⠁⠀⠈⠉⠛⠷⣮⣄⠤⣰⠟⠁⠀⠀⠈⠻⣷⣷⠟⠛⠛⠛⢻⡎⠘⠥⠋⢯⠹⡙⢯⡛⠿⠿⢿⠿⡿⢿⡿⣿⢿⡿⢿⠿⢿⠻⣟⠻⢏⠛⠜⠣⠉⠜⠁⠈⢀⣾⠛⠛⠛⠶⣥⣯⣾⠟⠛⠛⠛⢿⡀⠀⠀⢀⡄⡲⢌⣙⢯⣟⠛⣻⣿⣿⢿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣧⣀⡁⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀⠙⡿⠋⠀⡀⡄⢤⡀⣰⣿⣧⠀⠀⠀⠀⠀⢻⡀⠀⠀⠀⠀⠁⠀⠈⠀⠉⠀⠁⠘⠀⠂⠐⠂⠈⠀⠉⠀⠁⠀⠀⠀⠈⠀⠀⠀⠀⠀⢀⡾⠁⠀⠀⠀⠀⣸⣿⡃⢀⠀⠀⠀⠈⢷⡀⣄⡾⠾⠛⠋⠉⠀⠈⠻⣿⣿⣏⣈⣾⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣼⣁⡳⣄⠀⠀⠀⠀⠀⣠⠞⠁⠀⠀⠙⠻⢶⣗⣼⣿⣿⡄⠀⢄⣢⣔⡈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣡⣆⠆⠠⠀⢠⣿⣿⡿⣌⢞⣰⣦⠀⠈⢳⡉⠀⠀⠀⠀⠀⠀⠀⠀⣹⡿⢿⣿⣿⣿⣾⣿⣯⣦⡀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣿⣿⡿⢿⡋⠉⣡⣴⣾⣿⠿⠉⠉⠒⠲⢶⣾⡇⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⠁⠀⠀⠀⠈⠹⢻⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠀⣾⠟⠋⠀⠀⠀⠀⣸⣿⢧⡿⠚⠉⠁⠀⠀⠀⠀⠳⡄⠀⠀⠀⠀⠀⣀⣴⣿⣷⣶⣮⡄⠉⢻⣿⣿⠿⣟⣦⡄⡀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⡈⠻⠐⣾⣿⣿⡃⠀⠀⠀⠀⠠⠀⠈⣷⣄⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡄⠀⠀⠀⠀⠀⣼⠇⠂⠄⣀⠀⠀⠀⠰⠖⠒⣲⣶⢾⣾⣖⣶⣿⣿⣿⣶⣞⠒⠄⠒⠂⠒⢀⣄⣰⢿⡄⠀⠀⠀⠀⠠⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⣽⣷⣶⡶⠖⠛⠋⠁⠀⠘⣿⣿⣿⣠⢾⣩⣿⣿⣿⢯⣻⣇⠀⠀⠀⠀\n⠀⠀⠀⢀⣤⣤⣶⣿⣿⣿⣿⣿⣟⡃⠀⢿⣅⠙⠛⢷⣦⣠⣤⣤⡀⠀⢀⣀⣹⡻⣷⣦⣤⣤⣤⣾⣿⣿⣿⣿⣄⣀⣀⣠⣿⣿⠀⠀⠀⠈⠙⣦⣤⢤⡖⠺⠉⠉⣩⣧⣌⣠⣈⣏⡋⣹⣿⡗⣶⣒⣶⣿⡿⣿⣿⣾⣿⣦⣀⣀⣀⣾⣿⣿⣿⣦⣀⠀⢀⣀⣀⣠⣴⣾⡿⡁⢀⠀⠀⠀⣀⣀⣴⣾⣿⢻⣿⡟⠀⢰⣿⣿⣿⣿⣿⣿⣀⠀⠀⠀")
+            sys.exit()
+        else:
+            screen[player_x][player_y] = 3
+
+
+
+        self.grid = screen
+        return maps[current_map], current_map
+
+
+class Sidescreen:
+    def __init__(self, passed_inventory, passed_dimensions):
+        self.dimensions = passed_dimensions
+        self.lives = 3
+        #self.inventory_screen = passed_inventory.inventory_screen
+        self.array = self.generate_array(passed_inventory.inventory_screen)
+    
+    def generate_array(self, inventory_screen):
+        #determine the section of screen to allocate for the inventory, paste the fetched inventory array into the allocated section
+        return np.zeros(self.dimensions)
+
 
 
 class Map:
@@ -79,23 +126,91 @@ class Map:
         self.dimensions = passed_height, passed_width
         self.array = self.generate_floor(passed_level)
     
+    
+    def generate_section(self, start, end, floor_number, shared_map_name):
+        height, width = self.dimensions
+        
+        existing_shared_map = multiprocessing.shared_memory.SharedMemory(name=shared_map_name)
+        shared_map_array = np.ndarray((height, width), dtype='int32', buffer=existing_shared_map.buf)
+
+        for y in range(start, end):
+            section_row = []
+            for x in range(width):
+                tile_color = 1
+                if (x - (width // 2)) ** 2 + ((y - (height // 2)) * 1.2) ** 2 <= 23 ** 2:
+                    tile_color = 2
+                    if (x - (width // 2)) ** 2 + ((y - (height // 2)) * 1.2) ** 2 <= 21.75 ** 2:
+                        if random(1, 30) == 1 and x != 22 and y != 30:
+                            tile_color = 9
+                        else:
+                            if noise(x, y, self) == 0:
+                                dark_tile, light_tile = 4, 6
+                                tile_color = (
+                                    (x - 1) % 2 == 0 and
+                                    (y % 2 == 0 and dark_tile or light_tile) or
+                                    (y % 2 == 0 and light_tile or dark_tile)
+                                )
+                            else:
+                                tile_color = 1
+                else:
+                    tile_color = 1
+                #place exit flag
+                if x == 30 and y == 40:
+                    tile_color = 8
+
+                section_row.append(tile_color)
+            shared_map_array[y, :] = section_row
+
+        existing_shared_map.close()
+
     def generate_floor(self, floor_number):
         height, width = self.dimensions
 
-        map_array = []
-        for y in range(height):
-            map_row = []
+        sections = 3
+        processes = []
+
+        shared_map = multiprocessing.shared_memory.SharedMemory(create=True, size=height * width * np.dtype('int32').itemsize)
+        shared_map_array = np.ndarray((height, width), dtype='int32', buffer=shared_map.buf)
+        shared_map_array[:] = 1  #start with a blank array of black cells
+
+        #divide the work into sections and spawn processes
+        section_height = height // sections
+        for section_index in range(sections):
+            start_row = section_index * section_height
+            end_row = start_row + section_height if section_index < sections - 1 else height
+
+            p = multiprocessing.Process(
+                target=self.generate_section,
+                args=(start_row, end_row, floor_number, shared_map.name)
+            )
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join()
+
+        result = shared_map_array.copy()
+
+        shared_map.close()
+        shared_map.unlink()
+
+        return result.tolist()
+    '''
+
+    def generate_section(self, start, end, floor_number, map_array):
+        height, width = self.dimensions
+        #section_array = []
+        for y in range(start, end):
+            section_row = []
             for x in range(width):
                 tile_color = 1
                 match floor_number:
-                    case 1: #
-                        #basic circle ----------------------------------------------------------------
-                        
+                    case 1: #basic circle ----------------------------------------------------------------
+
                         if (x-(width // 2))**2 + ((y-(height // 2)) * 1.2)**2 <= 23**2:
                             tile_color = 2
                             if (x-(width // 2))**2 + ((y-(height // 2)) * 1.2)**2 <= 21.75**2:
                                 if noise(x, y, self) == 0:
-                                    #additional obstacles
                         
                                     dark_tile, light_tile = 4, 6
                                     tile_color = (x - 1) % 2 == 0 and (y % 2 == 0 and dark_tile or light_tile) or (y % 2 == 0 and light_tile or dark_tile)
@@ -103,26 +218,43 @@ class Map:
                                     tile_color = 1
                         else:
                             tile_color = 1
-                        #-----------------------------------------------------------------------------
-                    case 2: #castle
-                        #-----------------------------------------------------------------------------
-                        if within_castle(x, y, 1.2, width // 2, height // 2, 22, 0): 
-                            #check for static objects (interior walls, doors)
 
-                            #render a checkerboard floor pattern
-                            tile_color = (x - 1) % 2 == 0 and (y % 2 == 0 and 2 or 5) or (y % 2 == 0 and 5 or 2)
-                        else:
-                            tile_color = 1 #black tile
-                        
-                        #-----------------------------------------------------------------------------
-                    case 3: #3 4 3 circle arrangement
-                        #-----------------------------------------------------------------------------
+                    case 2: #-----------------------------------------------------------------------------
                         pass
-                        #-----------------------------------------------------------------------------
-                map_row += [tile_color]
-            map_array += [map_row]
-        return map_array
-    
+                    case 3: #-----------------------------------------------------------------------------
+                        pass
+                section_row += [tile_color]
+            #map_array += [section_row]
+            #print(y)
+            #print(map_array)
+            #print(section_row)
+            map_array[y] = section_row
+            #sys.exit()
+        #return map_array
+
+    def generate_floor(self, floor_number):
+        height, width = self.dimensions
+        
+
+        sections = 3
+        processes = []
+
+        #shared array all processes write to
+        shared_map_array = np.ones(height, dtype=int).tolist()
+        #map_array = np.ones(height, dtype=int).tolist()
+
+        for i in range(0, height + 0, height // sections):
+            i += height // sections
+            #p = multiprocessing.Process(target=self.generate_section, args=(i - height // sections, i, floor_number, shared_map_array))
+            #processes.append(p)
+            #p.start()
+            self.generate_section(i - height // sections, i, floor_number, shared_map_array)
+
+        #for p in processes:
+            #p.join()
+
+        return shared_map_array
+    '''
 
 class Player:
     def __init__(self, passed_inventory):
@@ -133,9 +265,10 @@ class Player:
         x, y = self.position #player position
         d_x, d_y = direction #direction x and y
 
+        #player class handles erasure of rock tiles
         unit_destination = (x + d_x, y + d_y)
         destination_material = map.array[unit_destination[0]][unit_destination[1]]
-        if destination_material in [4, 6]: #destination is not rock, take a single step
+        if destination_material in [4, 6, 8, 9]: #destination is not rock, take a single step
             self.position = unit_destination
             convert_to_floor_tile(x, y, map.array)
         elif destination_material == 1:#drill into rock until anything but tiles[1] is reached
@@ -143,7 +276,7 @@ class Player:
             while True:
                 destination = (x + (d_x * scale_factor), y + (d_y * scale_factor))
                 destination_material = map.array[destination[0]][destination[1]]
-                if destination_material in [4, 6]: #end of rock has been reached
+                if destination_material in [4, 6, 8, 9]: #end of rock has been reached
                     self.position = destination
                     #convert the final rock cell into a floor cell
                     tunnel_x, tunnel_y = (x + (d_x * (scale_factor - 1)), y + (d_y * (scale_factor - 1)))
@@ -155,7 +288,6 @@ class Player:
                     convert_to_floor_tile(destination_x, destination_y, map.array)
                     break
                 else:
-                    #print("rock!", x, y)
                     #still drilling through rock, replace rock with ground
                     tunnel_x, tunnel_y = (x + (d_x * (scale_factor - 1)), y + (d_y * (scale_factor - 1)))
                     convert_to_floor_tile(tunnel_x, tunnel_y, map.array)
@@ -172,5 +304,10 @@ class Inventory:
         self.items = []
         self.inventory_screen = self.generate_inventory_screen()
 
-    def generate_inventory_screen(self):
+    def generate_inventory_screen(self): #generate the inventory part of the sidescreen
         return
+
+
+class NPC: #enemies & merchants
+    def __init__(self, passed_type):
+        self.type = passed_type
--- main.py
+++ main.py
@@ -4,107 +4,110 @@ from random import randint as random
 import os
 import time
 import shutil #for terminal size check
+from getch import getch
 
 
-#inital terminal sizing configuration -----------------------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    #inital terminal sizing configuration -------------------------------------------------------------------
 
-accepted_columns, accepted_lines = 160, 35
-def validate_terminal_dimensions(accepted_columns, accepted_lines):
-    columns, lines = shutil.get_terminal_size()
-    if (columns < accepted_columns) or (lines < accepted_lines):
-        print((columns < accepted_columns) and f"terminal width is too narrow ({columns}/{accepted_columns}), make it wider" or f"terminal height is too low ({lines}/{accepted_lines}), make it taller")
-        time.sleep(0.5)
+    accepted_columns, accepted_lines = 160, 44
+    def validate_terminal_dimensions(accepted_columns, accepted_lines):
+        columns, lines = shutil.get_terminal_size()
+        if (columns < accepted_columns) or (lines < accepted_lines):
+            print((columns < accepted_columns) and f"terminal width is too narrow ({columns}/{accepted_columns}), make it wider" or f"terminal height is too low ({lines}/{accepted_lines}), make it taller")
+            time.sleep(0.5)
+            os.system('clear')
+            return False
+        else:
+            os.system('clear')
+            return True
+
+    while True:
         #os.system('clear')
-        return False
-    else:
-        os.system('clear')
-        return True
-
-while True:
-    os.system('clear')
-    if validate_terminal_dimensions(accepted_columns, accepted_lines): break
+        if validate_terminal_dimensions(accepted_columns, accepted_lines): break
 
 
-#settings -------------------------------------------------------------------------------------------------------------------------------------------------------------
+    #settings ------------------------------------------------------------------------------------------------
 
-#height and width of the screen
-height, width = 45, 80
+    #height and width of the screen
+    height, width = 45, 80
 
-#height and width of the map
-map_height = 45
-map_width = 60
+    #height and width of the map
+    map_height = 45
+    map_width = 60
 
-fps = 5
+    fps = 5
 
-#instance configuration ------------------------------------------------------------------------------------------------------------------------------------------------
-inventory = Inventory()
-player = Player(inventory)
-map_one = Map(map_height, map_width, 1)
+    #instance configuration -----------------------------------------------------------------------------------
+    inventory = Inventory()
+    player = Player(inventory)
 
-#main loop -------------------------------------------------------------------------------------------------------------------------------------------------------------
+    maps = [Map(map_height, map_width, i + 1) for i in range(5)]
 
-while True:
-    #clear the last screen
-    os.system('clear')
-
-    #validate terminal dimensions (disabled for performance)
-    #if not validate_terminal_dimensions(accepted_columns, accepted_lines): continue
-
-    #generate instances -------------------------------------------------------------------------
-
-    screen = Screen(height, width)
+    #ticket = time.time()
+    map_one = maps[0] #Map(map_height, map_width, 1)
+    #print((time.time() - ticket)*1000)#22ms with multiprocessing, 6ms without
+    #sys.exit()
     map = map_one
+    current_map = 0
 
-    #display elements ---------------------------------------------------------------------------
+    #main loop -------------------------------------------------------------------------------------------------
 
-    screen.add_elements_to_screen([map, player, 1, 1, 1, 1])#error origin
-    screen.display()
+    while True:
+        #clear the last screen
+        os.system('clear')
 
-    #break
+        #validate terminal dimensions
+        if not validate_terminal_dimensions(accepted_columns, accepted_lines): continue
 
-    #keybinds -----------------------------------------------------------------------------------
+        #generate instances -------------------------------------------------------------------------
 
-    pressed_key = input()
-    match pressed_key.lower():
-        #movement keybinds-----------------------------
-        #top left is (0, 0)
-        case "w":
-            player.move(np.array([-1, 0]), map)
-        case "a":
-            player.move(np.array([0, -1]), map)
-        case "s":
-            player.move(np.array([1, 0]), map)
-        case "d":
-            player.move(np.array([0, 1]), map)
+        screen = Screen(height, width)
 
-        #action keybinds-------------------------------
-        
-        case "e": #interacting with items
-            pass
-        case "y": #"yes" response to npcs
-            pass
-        case "n": #"no" response to npcs
-            pass
-        case " ": #attack
-            pass
+        #sidescreen instances --------------
+        inventory = Inventory()
 
-        #end the program-------------------------------
+        sidescreen = Sidescreen(inventory, (45, 20))
 
-        case "q":
-            break
+        #display elements ---------------------------------------------------------------------------
+        #next frame's map depends on if player steps on a flag
+        map, current_map = screen.add_elements_to_screen([map, player, sidescreen, 1, 1, 1], maps, current_map) #0 = map, 1 = player, 2 = sidescreen
+        screen.display()
+        print(f"level: {current_map}")
+        #break
 
-    #wait the refresh period
-    time.sleep(fps**-1)
+        #keybinds -----------------------------------------------------------------------------------
 
+        pressed_key = getch()
+        match pressed_key.lower():
+            #movement keybinds-----------------------------
+            #top left is (0, 0)
+            case "w":
+                player.move(np.array([-1, 0]), map)
+            case "a":
+                player.move(np.array([0, -1]), map)
+            case "s":
+                player.move(np.array([1, 0]), map)
+            case "d":
+                player.move(np.array([0, 1]), map)
+                '''
+            #action keybinds-------------------------------
+            
+            case "e": #interacting with items
+                pass
+            case "y": #"yes" response to npcs
+                pass
+            case "n": #"no" response to npcs
+                pass
+            case " ": #attack
+                pass
+            '''
+                #end the program-------------------------------
+            case "q":
+                break
 
-
-
-
-
-
-
-
-
+        #wait the refresh period
+        time.sleep(fps**-1)
 
 
 
